4d722f6158be3569058bf1b0792371ea9f3f8837
Kevin Rama <ramanantsoa.vlm@gmail.com>
2022-08-02 11:19:28 +0200

    landing page responsive

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import Landing_page from 'Images/Landing-page.png';
 import 'Pages/Landing/Landing.css';
 
 const Landing = () => {
     return (
-        <>
-            <img  className='Landing' src={Landing_page} alt='logo' />
-        </>
+        <div style={{ 
+            backgroundImage: `url("https://images.unsplash.com/photo-1506241537529-eefea1fbe44d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")`,
+            backgroundSize: 'cover',
+            backgroundPosition: 'center',
+            height: '100vh', 
+          }}>
+            <div className='title'>
+            <h1>Charles Cantin - Photographie</h1>
+            </div>
+            
+          </div>
     )
 }
 
