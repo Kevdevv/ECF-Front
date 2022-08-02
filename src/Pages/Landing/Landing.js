cd2c57c972c5cb5c4dab2bf4a13bb6520247b650
Kevin Rama <ramanantsoa.vlm@gmail.com>
2022-08-02 13:42:57 +0200

    retouche responsive, home et categorie

--- src/Pages/Landing/Landing.js
+++ src/Pages/Landing/Landing.js
@@ -7,12 +7,11 @@ const Landing = () => {
             backgroundImage: `url("https://images.unsplash.com/photo-1506241537529-eefea1fbe44d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80")`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
-            height: '100vh', 
+            height: '100vh',
           }}>
             <div className='title'>
             <h1>Charles Cantin - Photographie</h1>
-            </div>
-            
+            </div>            
           </div>
     )
 }
